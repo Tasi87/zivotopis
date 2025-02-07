37884d1b577c8933373aea5ab578e2f28d3daf56
Tasi <tasi87@gmail.com>
2025-02-07 12:43:13 +0100

    Zadefinovanie TheNavigation.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
+// styles
+
+// components
+import TheNavigation from './components/TheNavigation'
+
+// views
+
+
+
 function App() {
   return (
     <div>
-		
+		<header>
+			<TheNavigation />
+		</header>
+
+		<main>
+			<h1>Tahssain Amir</h1>
+		</main>
+
+		<footer>
+
+		</footer>
     </div>
   );
 }
