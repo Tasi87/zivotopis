158fff9ca2de6ac97e92538c0df8ee7757acb155
Tasi <tasi87@gmail.com>
2025-02-15 14:17:24 +0100

    uprava textu v about me

--- src/components/tabs/AboutMe.tsx
+++ src/components/tabs/AboutMe.tsx
@@ -3,7 +3,7 @@ const AboutMe = () => {
 		<div>
 			<h2>O mne</h2>
 			<p>
-				Rád sa vzdelávam, učím novým zručnostiam a potom ich odovzdávam iným. Som komunikatívny typ, čoho dôkazom je práca s deťmi aj dospelými (tréner, inštruktor).Veľa komunikujem s ľuďmi, dôkazom toho je moja práca s dospelými ako inštruktor a s deťmi ako tréner vo voľnom čase. Mojou záľubou je skalné lezenie, horolezectvo, pobyt v prírode, inštruktorovanie a trénovanie, čítanie, cestovanie, záujem o kinematografiu, spoločenské alebo počítačové hry.
+				Rád sa vzdelávam, učím novým zručnostiam a potom ich odovzdávam iným. Som komunikatívny typ, čoho dôkazom je práca s deťmi aj dospelými (tréner, inštruktor). Mojou záľubou je športové lezenie na skalách, horolezectvo, pobyt v prírode, inštruktorovanie a trénovanie, čítanie, cestovanie, záujem o kinematografiu, spoločenské alebo počítačové hry.
 			</p>
 		</div>
 	);
